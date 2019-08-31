3f2ee4462fd83bbf85efe2ddac3b35c1199045c9
Oliver Walters <captainwalters@github.com>
2019-08-31 23:33:19 +0100

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,57 @@
 const gulp        = require('gulp');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
 const php         = require('gulp-connect-php');
 
+// Copying html
 gulp.task('html', function() {
-    return gulp.src(['./src/**/*.html'])
+    return gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist'))
         .pipe(browserSync.stream());
 });
 
+// Copying php
 gulp.task('php', function() {
-    return gulp.src(['./src/**/*.php'])
+    return gulp.src('./src/**/*.php')
         .pipe(gulp.dest('./dist'))
         .pipe(browserSync.stream());
 });
 
+// Compile sass to css into and inject live changes
 gulp.task('sass', function () {
-    return gulp.src(['./src/scss/*.scss'])
+    return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
+// Copying JS
 gulp.task('js', function() {
-    return gulp.src(['./src/js/*.js'])
+    return gulp.src('./src/js/**/*.js')
         .pipe(gulp.dest('./dist/js'))
-        .pipe(browserSync.stream());
 });
 
+// Copying fonts 
+gulp.task('fonts', function() {
+    return gulp.src('./src/fonts/**/*')
+      .pipe(gulp.dest('dist/fonts'))
+});
+
+// So the thing
 gulp.task('serve', gulp.series('html','php','sass','js', function() {
     php.server({
         port:8000,
         base:'./dist/',
         keepalive: true
     }, function() {
-        browserSync({
+        browserSync.init({
             proxy: '127.0.0.1:8000'
         });
     });
     gulp.watch('./src/**/*.html', gulp.series('html')).on('change', browserSync.reload);
     gulp.watch('./src/**/*.php', gulp.series('php')).on('change', browserSync.reload);
-    gulp.watch('./src/scss/*.scss', gulp.series('sass')).on('change', browserSync.reload);
-    gulp.watch('./src/js/*.js', gulp.series('js'))
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('./src/js/**/*.js', gulp.series('js')).on('change', browserSync.reload);
 }));
 
 gulp.task('default', gulp.parallel('serve'));
