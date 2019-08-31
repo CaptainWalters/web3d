8792a413afc42f0f2b8899c7d5e9980f09bd2208
Oliver Walters <captainwalters@github.com>
2019-08-31 20:42:31 +0100

    php-connect works

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync');
 const sass        = require('gulp-sass');
+const php         = require('gulp-connect-php');
 
 gulp.task('html', function() {
     return gulp.src(['./src/**/*.html'])
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('php', function() {
+    return gulp.src(['./src/**/*.php'])
         .pipe(gulp.dest('./dist'))
         .pipe(browserSync.stream());
 });
@@ -16,18 +23,25 @@ gulp.task('sass', function () {
 });
 
 gulp.task('js', function() {
-    return gulp.src(['src/js/*.js'])
+    return gulp.src(['./src/js/*.js'])
         .pipe(gulp.dest('./dist/js'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', gulp.series('html', 'sass', 'js', function() {
-    browserSync.init({
-        server: './dist/'
+gulp.task('serve', gulp.series('html','php','sass','js', function() {
+    php.server({
+        port:8000,
+        base:'./dist/',
+        keepalive: true
+    }, function() {
+        browserSync({
+            proxy: '127.0.0.1:8000'
+        });
     });
-    gulp.watch('./scss/*.scss', gulp.series('sass'));
-    gulp.watch('./js/*.js', gulp.series('js'))
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./src/**/*.html', gulp.series('html')).on('change', browserSync.reload);
+    gulp.watch('./src/**/*.php', gulp.series('php')).on('change', browserSync.reload);
+    gulp.watch('./src/scss/*.scss', gulp.series('sass')).on('change', browserSync.reload);
+    gulp.watch('./src/js/*.js', gulp.series('js'))
 }));
 
 gulp.task('default', gulp.parallel('serve'));
