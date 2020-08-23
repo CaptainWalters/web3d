4d22aa41f0e6b98ce0fb9c692528501ee22154b6
Oliver Walters <captainwalters@github.com>
2020-08-23 14:52:54 +0100

    updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,65 @@
-const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
-const php         = require('gulp-connect-php');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect-php');
+const clean = require('gulp-rimraf');
+const sass = require('gulp-sass');
+
+gulp.task('clean', () => gulp.src('build/*', { read: false }).pipe(clean()));
 
 // Copying html
-gulp.task('html', function() {
-    return gulp.src('./src/**/*.html')
-        .pipe(gulp.dest('./build'))
-        .pipe(browserSync.stream());
-});
+gulp.task('html', () =>
+  gulp.src('./src/**/*.html').pipe(gulp.dest('./build')).pipe(browserSync.stream())
+);
 
 // Copying php
-gulp.task('php', function() {
-    return gulp.src('./src/**/*.php')
-        .pipe(gulp.dest('./build'))
-        .pipe(browserSync.stream());
-});
+gulp.task('php', () =>
+  gulp.src('./src/**/*.php').pipe(gulp.dest('./build')).pipe(browserSync.stream())
+);
 
 // Compile sass to css into and inject live changes
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./build/css'))
-        .pipe(browserSync.stream());
+gulp.task('sass', () => {
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 // Copying JS
-gulp.task('js', function() {
-    return gulp.src('./src/js/**/*.js')
-        .pipe(gulp.dest('./build/js'))
-});
+gulp.task('js', () =>
+  gulp.src('./src/js/**/*.js').pipe(concat('all.js')).pipe(gulp.dest('./build/js'))
+);
 
-gulp.task('assets', () => (
-    gulp.src('./src/assets/**/*')
-        .pipe(gulp.dest('./build/assets'))
-))
+gulp.task('assets', () => gulp.src('./src/assets/**/*').pipe(gulp.dest('./build/assets')));
 
-gulp.task('db', () => (
-    gulp.src('./src/db/*')
-        .pipe(gulp.dest('./build/db'))
-))
+gulp.task('db', () => gulp.src('./src/db/*').pipe(gulp.dest('./build/db')));
 
-// Copying fonts 
-gulp.task('fonts', function() {
-    return gulp.src('./src/fonts/**/*')
-        .pipe(gulp.dest('./build/fonts'))
-});
+// Copying fonts
+gulp.task('fonts', () => gulp.src('./src/fonts/**/*').pipe(gulp.dest('./build/fonts')));
 
 // So the thing
-gulp.task('serve', gulp.series('html','php','sass','js','fonts','assets','db', function() {
-    php.server({
-        port:8000,
-        base:'./build/',
-        keepalive: true
-    }, function() {
+gulp.task(
+  'serve',
+  gulp.series('clean', 'html', 'php', 'sass', 'js', 'fonts', 'assets', 'db', function () {
+    connect.server(
+      {
+        port: 8000,
+        base: './build/',
+        keepalive: true,
+      },
+      () => {
         browserSync.init({
-            proxy: '127.0.0.1:8000'
+          proxy: 'localhost:8000/index.php',
+          notify: false,
         });
-    });
+      }
+    );
     gulp.watch('./src/**/*.html', gulp.series('html')).on('change', browserSync.reload);
     gulp.watch('./src/**/*.php', gulp.series('php')).on('change', browserSync.reload);
     gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
     gulp.watch('./src/js/**/*.js', gulp.series('js')).on('change', browserSync.reload);
-}));
+  })
+);
 
 gulp.task('default', gulp.parallel('serve'));
--- src/index.php
+++ src/index.php
@@ -82,6 +82,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
     <!-- Custom stuff -->
-    <script type="text/javascript" src="./js/home.js"></script>
+    <script type="text/javascript" src="./js/all.js"></script>
 </body>
 </html>
